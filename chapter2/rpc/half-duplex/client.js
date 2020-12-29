ccb0404998e5ea30cdbfd55b6d81d307992aa14c
刘显志 <1010465852@qq.com>
2020-12-29 11:32:55 +0800

    feat:chapter2

---
+++ chapter2/rpc/half-duplex/client.js
@@ -0,0 +1,52 @@
+const net = require('net');
+
+// 创建socket
+const socket = new net.Socket({})
+
+// 连接服务器
+socket.connect({
+  host: 'localhost',
+  port: 4000
+})
+
+const lessonids = [
+  "136797",
+  "136798",
+  "136799",
+  "136800",
+  "136801",
+  "136803",
+  "136804",
+  "136806",
+  "136807",
+  "136808",
+  "136809",
+  "141994",
+  "143517",
+  "143557",
+  "143564",
+  "143644",
+  "146470",
+  "146569",
+  "146582"
+]
+
+let id = Math.floor(Math.random() * lessonids.length)
+
+// 往服务器传数据
+socket.write(encoded(id))
+
+socket.on('data', function(buffer){
+  console.log(buffer.toString());
+
+  // 接收到数据之后，按照半双工通信的逻辑，马上开始下一次请求
+  id = Math.floor(Math.random() * lessonids.length)
+  socket.write(encoded(id))
+})
+
+// 把编码请求包的逻辑封装为一个函数
+function encoded(id) {
+  buffer = Buffer.alloc(4)
+  buffer.writeInt32BE(lessonids[id])
+  return buffer
+}
